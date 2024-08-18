bb0916ec3a2ec5b8c310b40e538d0e3ac00cac54
OLAMIDE ADEOLA <150748814+Codexcomm@users.noreply.github.com>
2024-08-18 17:34:14 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+let user_age = Number(prompt("Pls Enter Your Age"))
+
+if(user_age >= 14){
+    let firstname = prompt("Pls Enter Your First Name")
+    let lastname = prompt('Pls Enter Your Last Name')
+    let course = prompt("Pls Enter The Chosen Course")
+    alert("Registration Successful ✔️" + firstname )
+}
+
+else{
+    alert("You Are Not Of Age To Register")
+}
